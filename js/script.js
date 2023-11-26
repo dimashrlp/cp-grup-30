2a497201012165f5560466cce6d4ae76fb2249aa
Dimas Harli Praetya <dimashp1014@gmail.com>
2023-11-26 21:36:05 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,30 @@ document.querySelector("#menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+document
+  .querySelectorAll(".dropdown-toggle")
+  .forEach(function (dropdownToggle) {
+    dropdownToggle.addEventListener("click", function () {
+      var dropdownMenu = this.parentElement.querySelector(".dropdown-menu");
+      toggleDropdown(dropdownMenu);
+    });
+  });
+
+// Menutup dropdown yang lain ketika suatu dropdown dibuka
+function toggleDropdown(dropdownMenu) {
+  document.querySelectorAll(".dropdown-menu").forEach(function (menu) {
+    if (menu !== dropdownMenu) {
+      menu.style.display = "none";
+    }
+  });
+
+  // Toggle dropdown yang dipilih
+  dropdownMenu.style.display =
+    dropdownMenu.style.display === "none" || dropdownMenu.style.display === ""
+      ? "block"
+      : "none";
+}
+
 // Toggle class active untuk search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
